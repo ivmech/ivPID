e48c7d52a10df4ce830cc49cb9b63d3c4550eec0
ivmech <bilgi@ivmech.com>
2015-12-18 06:02:05 +0200

    first release

---
+++ PID.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+#
+# This file is part of IvPID.
+# Copyright (C) 2015 Ivmech Mechatronics Ltd. <bilgi@ivmech.com>
+#
+# IvPID is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IvPID is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#title           :PID.py
+#description     :python pid controller
+#author          :Caner Durmusoglu
+#date            :20151218
+#version         :0.1
+#notes           :
+#python_version  :2.7
+#==============================================================================
+# Python PID Controller
+# Simple implementation of a Proportional-Integral-Derivative (PID) Controller.
+# PID controller gives output value for error between desired reference input and measurement feedback to minimize error value.
+# More information: http://en.wikipedia.org/wiki/PID_controller
+#
+
+import time
+
+class PID:
+    """
+	PID Controller
+	"""
+
+    def __init__(self, P=2.0, I=0.0, D=1.0):
+
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+
+        self.sample_time = 0.01
+        self.current_time = time.time()
+        self.last_time = self.current_time
+
+        self.clear()
+
+    def clear(self):
+        self.SetPoint = 0.0
+
+        self.Cp = 0.0
+        self.Ci = 0.0
+        self.Cd = 0.0
+        self.last_error = 0.0
+
+        # Windup Guard
+        self.int_error = 0.0
+        self.windup_guard = 20.0
+
+        self.output = 0.0
+
+    def update(self, feedback_value):
+        """
+		Calculate PID output value for given reference input and feedback
+		"""
+        error = self.SetPoint - feedback_value
+
+        self.current_time = time.time()
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if (delta_time >= self.sample_time):
+            self.Cp = self.Kp * error
+            self.Ci += error * delta_time
+
+            if (self.Ci < -self.windup_guard):
+                self.Ci = -self.windup_guard
+            elif (self.Ci > self.windup_guard):
+                self.Ci = self.windup_guard
+
+            self.Cd = 0.0
+            if delta_time > 0:
+                self.Cd = delta_error / delta_time
+
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.Cp + (self.Ki * self.Ci) + (self.Kd * self.Cd)
+
+    def setKp(self, P):
+        self.Kp = P
+
+    def setKi(self, I):
+        self.Ki = I
+
+    def setKd(self, D):
+        self.Kd = D
+
+    def setWindup(self, W):
+        self.windup_guard = W
+
+    def setSampleTime(self, sample_time):
+        self.sample_time = sample_time
---
+++ test_pid.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+#
+# This file is part of IvPID.
+# Copyright (C) 2015 Ivmech Mechatronics Ltd. <bilgi@ivmech.com>
+#
+# IvPID is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IvPID is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#title           :test_pid.py
+#description     :python pid controller test
+#author          :Caner Durmusoglu
+#date            :20151218
+#version         :0.1
+#notes           :
+#python_version  :2.7
+#dependencies    : matplotlib, numpy, scipy
+#==============================================================================
+
+import PID
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import spline
+
+def test_pid(P = 0.2, D= 0.0, I = 0.0, L=100):
+    pid = PID.PID(P, I, D)
+
+    pid.SetPoint=0.0
+    pid.setSampleTime(0.01)
+
+    END = L
+    feedback = 0
+
+    feedback_list = []
+    time_list = []
+    setpoint_list = []
+
+    for i in range(1, END):
+        pid.update(feedback)
+        output = pid.output
+        if pid.SetPoint > 0:
+            feedback += (output - (1/i))
+        if i>9:
+            pid.SetPoint = 1
+        time.sleep(0.02)
+
+        feedback_list.append(feedback)
+        setpoint_list.append(pid.SetPoint)
+        time_list.append(i)
+
+    time_sm = np.array(time_list)
+    time_smooth = np.linspace(time_sm.min(), time_sm.max(), 300)
+    feedback_smooth = spline(time_list, feedback_list, time_smooth)
+
+    plt.plot(time_smooth, feedback_smooth)
+    plt.plot(time_list, setpoint_list)
+    plt.xlim((0, L))
+    plt.ylim((min(feedback_list)-0.5, max(feedback_list)+0.5))
+
+    plt.ylim((1-0.5, 1+0.5))
+
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    test_pid(1.2, 0.001, 1, L=50)
+#    test_pid(0.8, L=50)
